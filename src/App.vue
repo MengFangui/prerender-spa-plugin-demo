f5055502e4dc7e6cd06afc6f71eafa367526ca9a
MengFangui <meng010387@126.com>
2018-07-10 10:17:09 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <p>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
+      <router-link to="/test">test</router-link>
+      <span @click="mm" class="mm">555</span>
+    </p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to your prerender-spa-plugin Vuejs 2.0 demo app!'
+    }
+  },
+  methods:{
+    mm(){
+      alert(55)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+.mm{
+  cursor: pointer;
+}
+</style>
---
+++ src/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p @click="testMethod" class="p">
+      test
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  methods:{
+    testMethod(){
+      alert('test')
+    }
+  }
+}
+</script>
+<style>
+.p{
+  cursor: pointer;
+}
+</style>
+
+
